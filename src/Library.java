a410358d3220d39a82cea69e2b4de096339f43d3
trahvex <ruben.sanz98@gmail.com>
2020-02-20 19:17:42 +0100

    library

--- src/Library.java
+++ src/Library.java
@@ -1,8 +1,17 @@
 
 
 public class Library {
-    public Book books;
+    public Book[] books;
+    public int nBooks;
     public int signuptime;
     public int maxscannedbooks;
+    public int libraryScore;
 
+
+    public void calScore(){
+        int aux = 0;
+        for (int i = 0; i < nBooks; i++)
+            aux += books[i].get_value();
+        libraryScore = aux;
+    }
 }
