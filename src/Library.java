12d37548058208b29ea3f5ff512858c477cbe259
trahvex <ruben.sanz98@gmail.com>
2020-02-20 20:40:44 +0100

    Library

--- src/Library.java
+++ src/Library.java
@@ -1,8 +1,12 @@
+import java.util.HashSet;
 import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Library {
     private int id;
-    private SortedSet<Book> libraryBooks;
+
+
+    private TreeSet<Book> libraryBooks;
     private int nBooks;
     private int signuptime;
     private int maxscannedbooks;
@@ -10,6 +14,13 @@ public class Library {
 
     public Library(int id, int nBooks, int signuptime, int maxscannedbooks) {
         this.id = id;
+        this.nBooks = nBooks;
+        this.signuptime = signuptime;
+        this.maxscannedbooks = maxscannedbooks;
+    }
+
+
+    public Library(int nBooks, int signuptime, int maxscannedbooks) {
         this.nBooks = nBooks;
         this.signuptime = signuptime;
         this.maxscannedbooks = maxscannedbooks;
@@ -43,7 +54,67 @@ public class Library {
         return false;
     }
 
-    public SortedSet<Book> getLibraryBooks() {
+
+    //return true if the library 1 is better than library 2
+    public boolean bestLibrary_a(Library l1, Library l2){
+        int l1Points, l2Points;
+        l1Points = l2Points = 0;
+        if(l1.getSignuptime() < l2.getSignuptime()) {
+            l1Points+=1;
+        }
+        else
+            l2Points+=1;
+
+        if(l1.getLibraryScore() < l2.getLibraryScore()) {
+            l1Points++;
+        }
+        else
+            l2Points+=1;
+
+        if(l1.getMaxscannedbooks() > l2.getMaxscannedbooks()) {
+            l1Points+=1;
+        }
+        else
+            l2Points+=1;
+
+        if(l1Points >= l2Points){
+            return true;
+        }
+        return false;
+    }
+
+    //return true if the library 1 is better than library 2
+    public boolean bestLibrary_b(Library l1, Library l2){
+        if(getSignuptime() < l2.getSignuptime()) {
+            return true;
+        }
+        return false;
+    }
+
+
+    //return true if the library 1 is better than library 2
+    public boolean bestLibrary_c(Library l1, Library l2){
+        int l1Points, l2Points;
+        l1Points = l2Points = 0;
+
+        int mediaPuntos1, mediaPuntos2;
+        mediaPuntos1 = mediaPuntos2 = 0;
+        //mediaPuntos1 = l1.getLibraryScore()/Main.days;
+        //mediaPuntos2 = l2.getLibraryScore()/Main.days;
+
+        //SUPONIENDO QUE EL NUMERO DE LIBROS QUE ENVIAN ES IGUAL
+        if(l1.getSignuptime() < l2.getSignuptime()) {
+            int daydif = l2.getSignuptime() - l1.getSignuptime();
+            if(mediaPuntos1*daydif > (l2.getLibraryScore()-l1.getLibraryScore())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+    public TreeSet<Book> getLibraryBooks() {
         return libraryBooks;
     }
 
